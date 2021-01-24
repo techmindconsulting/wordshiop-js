57c5a45afff1164c71fed6f7f8011192a63720e7
sfinx13 <saidi.ahamada@gmail.com>
2021-01-24 12:20:36 +0100

    update

--- conversion.js
+++ conversion.js
@@ -1,14 +1,15 @@
 /* Fonction qui est appelé sur l'évenement utilisateur onClick */
+
 function convertir() {
    /* Déclaration de variable */
-    const CURRENCY_DOLLAR = 1.2077;
-    let resultat;
+    const CURRENCY_DOLLAR = 1.2077; // TAUX DE CHANGE
+
+    // Parcourir le DOM (Document Object Model) afin de récuperer un élement HTML
+    // Récupérer un élément HTML à l'aide d'un ID (Ex: amount)
     let amount = document.getElementById('amount').value;
 
-    /* Opération de converssion */
-    resultat = amount * CURRENCY_DOLLAR;
-
     /* Affichage dans le document HTML */
-    document.getElementById('resultat').innerText = resultat + ' $';
+    // '+' Concaténation
+    let resulat = amount * CURRENCY_DOLLAR;
+    document.getElementById('resultat_us').innerText = resulat + ' $';
 }
-
