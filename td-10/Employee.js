fe12ff6a197929fdf8dd3f933e48076326223949
techmindconsulting <techmind.consulting.sasu@gmail.com>
2021-06-12 00:42:21 +0200

    update arborescence

--- td-10/Employee.js
+++
@@ -1,9 +0,0 @@
-class Employee {   
-    constructor(fullname = "", email = "", department = "common") {
-        this.fullname = fullname;
-        this.email = email;
-        this.department = department;
-    }
-}
-
-export default Employee;
